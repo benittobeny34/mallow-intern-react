1c58069f9ecfdb93be4fe745d9d32af278dd30e1
Benitto_raj <benittoraj@mallow-tech.com>
2022-05-10 22:17:46 +0530

    antd examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, DatePicker, version} from 'antd';
+import "antd/dist/antd.css";
+import DemoLine from "./components/DemoLine.js";
+import {DeleteOutlined,CloseCircleOutlined,AlignRightOutlined, DragOutlined } from '@ant-design/icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>antd version: {version}</h1>
+            <DatePicker/>
+            <Button type="primary" size="small" loading="true" style={{marginLeft: 8, fontSize: 7}}>
+                Primary Button
+            </Button>
+            <div className="w-1/4 flex flex-row justify-center items-center">
+                <DemoLine/>
+            </div>
+            <div className="flex justify-center py-8 my-8">
+                <DeleteOutlined />
+                <CloseCircleOutlined />
+                <AlignRightOutlined />
+                <DragOutlined />
+            </div>
+        </div>
+    );
 }
 
 export default App;
