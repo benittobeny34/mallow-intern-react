9907e1ce52545d8b5f99ce8a90c291c879d443a2
Benitto_raj <benittoraj@mallow-tech.com>
2022-06-04 18:18:19 +0530

    react-redux with react-hooks - way simpler than connect

---
+++ src/components/HookCakeContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {buyCake} from "../redux/cake/CakeActions";
+function HookCakeContainer() {
+   const numOfCakes =  useSelector(state => state.numOfCakes)
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <h2>No of Cakes - {numOfCakes} </h2>
+            <button onClick={() => {
+                dispatch(buyCake())
+            }}>Buy Cake</button>
+        </div>
+    )
+}
+
+export default HookCakeContainer;
